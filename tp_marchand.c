49e66d7fe56bdc8d0383b85f66ca03aac05ebc37
Alex-GD12 <59962597+Alex-GD12@users.noreply.github.com>
2020-02-03 11:22:00 +0100

    Update tp_marchand.c

--- tp_marchand.c
+++ tp_marchand.c
@@ -2,11 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 
+struct Objet{
 
+	char nom [30];
+	int prix;
+
+};	
+
+typedef struct Objet item;
 
 int main () {
 	
+	item inventaire [5] = { {"Briquet", 10}, {"Chemise", 3}, {"Pentalon", 4}, {"bottes", 6}, {"Rapiere", 50} };
+	printf ("INVENTAIRE : \n");
 	
+	for (int i = 0; i<5; i++) {
+		printf ("\n%s", inventaire[i].nom);
+		printf (" | PRIX : %d\n", inventaire[i].prix);
+		
+		
+
+	}
+	
+	
+
 	
 	
 	
