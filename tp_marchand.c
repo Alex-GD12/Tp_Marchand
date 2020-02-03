23bd41fa3949d17aa8bcf89cf02ba56945e676ac
Alex-GD12 <59962597+Alex-GD12@users.noreply.github.com>
2020-02-03 11:49:25 +0100

    Update tp_marchand.c

--- tp_marchand.c
+++ tp_marchand.c
@@ -12,20 +12,23 @@ struct Objet{
 typedef struct Objet item;
 
 void viewInventory (item * inventaire) {
-	printf ("INVENTAIRE : \n");
+	printf ("\n*************************************************************************************************************\n");
+	printf ("\nVOTRE INVENTAIRE : \n");
+	printf ("___________________\n");
 	for (int i = 0; i<5; i++) {
 		printf ("\n%s", inventaire[i].nom);
 		printf (" | PRIX : %d\n", inventaire[i].prix);
 		
 	}
+	printf ("\n*************************************************************************************************************\n");
 };
 
 int main () {
 	
-	item inventaire [5] = { {"Briquet", 10}, {"Chemise", 3}, {"Pentalon", 4}, {"bottes", 6}, {"Rapiere", 50} };
+	item inventaireJoueur [5] = { {"Briquet", 10}, {"Chemise", 3}, {"Pentalon", 4}, {"bottes", 6}, {"Rapiere", 50} };
 	
 	
-	viewInventory (&inventaireJoueur);
+	viewInventory (&inventaireJoueur[0]);
 	
 	
 	
