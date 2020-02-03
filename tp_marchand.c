e213bc135844628c500c3d86af493bc70cd4c13b
Alex-GD12 <59962597+Alex-GD12@users.noreply.github.com>
2020-02-03 12:06:37 +0100

    Update tp_marchand.c

--- tp_marchand.c
+++ tp_marchand.c
@@ -2,42 +2,61 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+//Structures
 struct Objet{
 
 	char nom [30];
 	int prix;
 
 };	
-
 typedef struct Objet item;
 
+
+//Fonctions 
 void viewInventory (item * inventaire) {
 	printf ("\n*************************************************************************************************************\n");
 	printf ("\nVOTRE INVENTAIRE : \n");
 	printf ("___________________\n");
+	
 	for (int i = 0; i<5; i++) {
-		printf ("\n%s", inventaire[i].nom);
-		printf (" | PRIX : %d\n", inventaire[i].prix);
+		if (strcmp(inventaire[i].nom , "") != 0){
+			printf ("\n%s", inventaire[i].nom);
+			printf (" | PRIX : %d\n", inventaire[i].prix);
+			
+		}
 		
 	}
 	printf ("\n*************************************************************************************************************\n");
 };
 
-int main () {
+void sortInventory (item * inventaire) {
+	int tamponPrix;
+	char tamponNom[30];
 	
-	item inventaireJoueur [5] = { {"Briquet", 10}, {"Chemise", 3}, {"Pentalon", 4}, {"bottes", 6}, {"Rapiere", 50} };
+	for (int i = 0; i<5; i++) {
+		printf ("\n%s", inventaire[i].nom);
+		printf (" | PRIX : %d\n", inventaire[i].prix);
+		
+	}
 	
 	
-	viewInventory (&inventaireJoueur[0]);
+	
+	
 	
 	
 	
 
+	printf ("\n*************************************************************************************************************\n");
+};
+
+
+//Début
+int main () {
 	
+	item inventaireJoueur [5] = { {"Briquet", 10}, {"Chemise", 3}, {"Pentalon", 4}, {"bottes", 6} };
 	
-	
-	
-	
+	viewInventory (&inventaireJoueur[0]);
 	
 	return 0;
 }
